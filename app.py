6b77effa673641e049d6ea6ab331e9584b186185
Evan Donkus <evdo5141@colorado.edu>
2024-03-14 12:44:11 +0000

    Commenting out next steps

--- app.py
+++ app.py
@@ -15,3 +15,24 @@ def db_conn:
     
     conn.close()
     return "Database Connection Successful"
+
+@app.route('/db_create')
+def db_create:
+    conn = psycopg2.connect("postgres://lab_10_db_9z0p_user:7j7l6zcs6J720w5CgLnvnPb8FAXjX01q@dpg-cnpel16n7f5s73c4pjeg-a/lab_10_db_9z0p")
+    
+    cur = conn.cursor()
+    
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS Basketball(
+                    First varchar(255),
+                    Last varchar(255),
+                    City varchar(255),
+                    Name varchar(255),
+                    Number int
+                    );
+                ''')
+    conn.commit()
+    
+    conn.close()
+    return "Basketball Table Successfully Created"
+    
