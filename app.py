b3fd05d783217b6cd6da9c15513d5546f95d8a9d
Evan Donkus <evdo5141@colorado.edu>
2024-03-14 13:14:28 +0000

    Fixing maybe

--- app.py
+++ app.py
@@ -88,8 +88,19 @@ def db_select():
     
 @app.route('/db_drop')
 def db_drop():
+    conn = psycopg2.connect("postgres://lab_10_db_9z0p_user:7j7l6zcs6J720w5CgLnvnPb8FAXjX01q@dpg-cnpel16n7f5s73c4pjeg-a/lab_10_db_9z0p")
     
+    cur = conn.cursor()
+    
+    cur.execute('''
+                DROP TABLE Basketball;
+                ''')
+    
+    conn.commit()
+    
+    conn.close()
         
+    return "Basketball Table Successfully Dropped"
         
         
         
