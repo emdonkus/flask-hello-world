1f95ff5c54325f128293097bb269b9da6dc16ac3
Evan Donkus <evdo5141@colorado.edu>
2024-03-14 13:01:36 +0000

    Added additional routes

--- app.py
+++ app.py
@@ -15,24 +15,83 @@ def testing():
     
     conn.close()
     return "Database Connection Successful"
-#
-#@app.route('/db_create')
-#def db_create:
-#    conn = psycopg2.connect("postgres://lab_10_db_9z0p_user:7j7l6zcs6J720w5CgLnvnPb8FAXjX01q@dpg-cnpel16n7f5s73c4pjeg-a/lab_10_db_9z0p")
+
+@app.route('/db_create')
+def db_create():
+    conn = psycopg2.connect("postgres://lab_10_db_9z0p_user:7j7l6zcs6J720w5CgLnvnPb8FAXjX01q@dpg-cnpel16n7f5s73c4pjeg-a/lab_10_db_9z0p")
     
-#    cur = conn.cursor()
+    cur = conn.cursor()
     
-#    cur.execute('''
-#                CREATE TABLE IF NOT EXISTS Basketball(
-#                    First varchar(255),
-#                    Last varchar(255),
- #                   City varchar(255),
-#                    Name varchar(255),
- #                   Number int
-#                    );
-#                ''')
-#    conn.commit()
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS Basketball(
+                    First varchar(255),
+                    Last varchar(255),
+                    City varchar(255),
+                    Name varchar(255),
+                    Number int
+                    );
+                ''')
+    conn.commit()
     
-#    conn.close()
- #   return "Basketball Table Successfully Created"
+    conn.close()
+    return "Basketball Table Successfully Created"
+    
+@app.route('/db_insert')
+def db_insert():
+    conn = psycopg2.connect("postgres://lab_10_db_9z0p_user:7j7l6zcs6J720w5CgLnvnPb8FAXjX01q@dpg-cnpel16n7f5s73c4pjeg-a/lab_10_db_9z0p")
+    
+    cur = conn.cursor()
+    
+    cur.execute('''
+                INSERT INTO Basketball (First, Last, City, Name, Number)
+                Values
+                ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+                ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+                ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+                ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+                ''')
+    conn.commit()
+    
+    conn.close()
+    
+    return "Basketball Table Populated"
+
+@app.route('/db_select')
+def db_select():
+    conn = psycopg2.connect("postgres://lab_10_db_9z0p_user:7j7l6zcs6J720w5CgLnvnPb8FAXjX01q@dpg-cnpel16n7f5s73c4pjeg-a/lab_10_db_9z0p")
+    
+    cur = conn.cursor()
+    
+    cur.execute('SELECT * FROM Basketball;')
+    
+    records = cur.fetchall()
+    
+    conn.close()
+    
+    response_string = ""
+    
+    response_string += "<table>"
+    
+    for player in records:
+        
+        response_String+="<tr>"
+        
+        for info in player:
+            
+            response_string+="<td>{}</td>".format(info)
+            
+        response_string+="</tr>"
+        
+    response_string+="</table>"
+    
+    return response_string
+    
+@app.route('/db_drop')
+def db_drop():
+    
+        
+        
+        
+        
+        
     
