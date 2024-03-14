39ffff7688a1d3aca72cd671c11efc3e150be1b9
Evan Donkus <evdo5141@colorado.edu>
2024-03-14 12:18:45 +0000

    Adding init db connection

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+import psycopg2
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World! from emdonkus in 3308'
+
+@app.route('/db_test')
+def hello_world():
+    conn = psycopg2.connect('postgres://lab_10_db_9z0p_user:7j7l6zcs6J720w5CgLnvnPb8FAXjX01q@dpg-cnpel16n7f5s73c4pjeg-a/lab_10_db_9z0p')
+    
+    
+    conn.close()
+    return "Database Connection Successful"
